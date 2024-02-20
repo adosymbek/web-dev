e16d192ba40ff3a21a3013e66f9f0365ad35be00
adosymbek <124169838+adosymbek@users.noreply.github.com>
2024-02-20 11:09:11 +0600

    Delete web dev/lab4 directory

--- web dev/lab4/date/3.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-current_datetime = datetime.now()
-
-current_datetime_without_microseconds = current_datetime.replace(microsecond=0)
-
-print("Datetime without microseconds:", current_datetime_without_microseconds)
--- web dev/lab4/generators/1.py
+++
@@ -1,9 +0,0 @@
-def generate_squares(N):
-    for i in range(1, N + 1):
-        yield i ** 2
-
-N = 5
-squares_generator = generate_squares(N)
-
-for square in squares_generator:
-    print(square)
